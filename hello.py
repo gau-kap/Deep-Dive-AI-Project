f4db141eca11223d0820938979acd40267433248
pravin-kapile <pravinappacc@gmail.com>
2026-07-20 10:03:56 -0500

    fastf1 practice

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello world this is gauri")
+import fastf1
+session = fastf1.get_session(2025, 13, "R" )
+session.load()
+lap = session.laps.pick_fastest()
+pos = lap.get_pos_data()
+
+circuit_info = session.get_circuit_info()
+print(circuit_info)
+session.load()
+print(session.laps.head(10))
+pia_laps = session.laps.pick_driver("Pia")
+print(pia_laps)
