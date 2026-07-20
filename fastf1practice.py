7bba4a8f415c00a369b522647d460c75e8de997f
gau-kap <gauri.p.kapile@gmail.com>
2026-07-20 11:08:27 -0500

    trying out different atributes for the fastf1 py package
    Added circuit information retrieval and Piastri lap data analysis.

--- fastf1practice.py
+++ fastf1practice.py
@@ -1,8 +1,28 @@
 import fastf1
+# load session
 session = fastf1.get_session(2025, 13, "R" )
 session.load()
 lap = session.laps.pick_fastest()
 pos = lap.get_pos_data()
+
+# get circuit info
+circuit_info = session.get_circuit_info()
+# print(circuit_info)
+session.load()
+# print(session.laps.head(10))
+
+# Piastri practice
+pia_laps = session.laps.pick_drivers("Pia")
+# print(pia_laps)
+
+# print(pia_laps["TyreLife"])
+print(session.laps.columns) 
+print(session.laps["Stint"])
+print(pia_laps["Stint"])
+print(pia_laps["TrackStatus"])
+
+print(session.weather_data)
+    
 
 circuit_info = session.get_circuit_info()
 print(circuit_info)
