07e389c9c1acc41718ddf98ccdb93e10b15e0995
gau-kap <gauri.p.kapile@gmail.com>
2026-07-20 11:54:11 -0500

    Fix merge conflicts in fastf1practice.py
    Resolved merge conflicts and cleaned up the code.

--- fastf1practice.py
+++ fastf1practice.py
@@ -1,21 +1,9 @@
 import fastf1
-<<<<<<< HEAD
-<<<<<<< HEAD
 # load session
-=======
->>>>>>> c7165fca6641c731dc875ce464e90957090c3e24
-=======
-# load session
->>>>>>> 7bba4a8f415c00a369b522647d460c75e8de997f
 session = fastf1.get_session(2025, 13, "R" )
 session.load()
 lap = session.laps.pick_fastest()
 pos = lap.get_pos_data()
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7bba4a8f415c00a369b522647d460c75e8de997f
 # get circuit info
 circuit_info = session.get_circuit_info()
 # print(circuit_info)
@@ -34,15 +22,9 @@ print(pia_laps["TrackStatus"])
 
 print(session.weather_data)
     
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 7bba4a8f415c00a369b522647d460c75e8de997f
 circuit_info = session.get_circuit_info()
 print(circuit_info)
 session.load()
 print(session.laps.head(10))
 pia_laps = session.laps.pick_driver("Pia")
 print(pia_laps)
->>>>>>> c7165fca6641c731dc875ce464e90957090c3e24
